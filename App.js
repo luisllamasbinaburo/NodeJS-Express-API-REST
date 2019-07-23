2ce78ff2af3a6b1421fe1e49ba16031b288adc79
Luis Llamas <luisllamas@hotmail.com>
2019-07-23 17:31:40 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 var express = require("express"); 
-//var cors = require('cors')
+var cors = require('cors')
 var app = express();
-//app.use(cors())
+app.use(cors())
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-//Ejemplo: GET http://localhost:8080/items
-app.get('/items', function(req, res, next) {
+//GetAll
+//Ejemplo: GET http://localhost:8080/item
+app.get('/item', function(req, res, next) {
   if(req.query.filter) {
 	next();
 	return;
@@ -17,37 +18,51 @@ app.get('/items', function(req, res, next) {
   console.log('Get all');
 });
 
-//Ejemplo: GET http://localhost:8080/items?filter=ABC
-app.get('/items', function(req, res) {
-  var filter = req.query.filter;
-  res.send('Get filter ' + filter);
-  console.log('Get filter ' + filter);
-});
-
-//Ejemplo: GET http://localhost:8080/items/10
-app.get('/items/:id', function(req, res, next) {
+//GetById
+//Ejemplo: GET http://localhost:8080/item/10
+app.get('/item/:id', function(req, res, next) {
   var itemId = req.params.id;
   res.send('Get ' + req.params.id);
   console.log('Get ' + req.params.id);
 });
 
-//Ejemplo: POST http://localhost:8080/items
-app.post('/items', function(req, res) {
+//GetFiltered
+//Ejemplo: GET http://localhost:8080/item?filter=ABC
+app.get('/item', function(req, res) {
+  var filter = req.query.filter;
+  res.send('Get filter ' + filter);
+  console.log('Get filter ' + filter);
+});
+
+//Create
+//Ejemplo: POST http://localhost:8080/item
+app.post('/item', function(req, res) {
 	var data = req.body.data;
 	res.send('Add ' + data);
 	console.log('Add ' + data);
 });
 
-//Ejemplo: PUT http://localhost:8080/items
-app.put('/items', function(req, res) {
-	var itemId = req.body.id;
+//Replace
+//Ejemplo: PUT http://localhost:8080/item/10
+app.put('/item/:id', function(req, res) {
+	var itemId = req.params.id;
+	var data = req.body.data;
+	res.send('Replacing ' + itemId + ' with ' + data);
+	console.log('Replacing ' + itemId + ' with ' + data);
+});
+
+//Update
+//Ejemplo: PATCH http://localhost:8080/item/10
+app.patch('/item/:id', function(req, res) {
+	var itemId = req.params.id;
 	var data = req.body.data;
 	res.send('Update ' + itemId + ' with ' + data);
 	console.log('Update ' + itemId + ' with ' + data);
 });
 
-//Ejemplo: DELETE http://localhost:8080/items
-app.delete('/items/:id', function(req, res) {
+//Delete
+//Ejemplo: DEL http://localhost:8080/items
+app.delete('/item/:id', function(req, res) {
 	var itemId = req.params.id;
 	res.send('Delete ' + itemId);
 	console.log('Delete ' + itemId);
